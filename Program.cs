573617d7f30b55dc16c6f2b3155331ee7e40275f
carlos_costa <carloscosta.pen@gmail.com>
2026-04-03 08:16:13 -0300

    Autoincrement

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Xml.XPath;
 
 namespace Datetables_app
 {
@@ -112,28 +113,61 @@ namespace Datetables_app
 
 
         }
+        static void CreatCoulmn(DataTable Table, Type Type, String Name, bool AutoIncrement, 
+             string Caption, bool ReadOnly, bool Unique, int IncremntSeed = 0)
+        {
+
+            DataColumn Dc = new DataColumn();
+            Dc.DataType = Type;
+            Dc.ColumnName = Name;
+            Dc.AutoIncrement = AutoIncrement;
+            Dc.AutoIncrementSeed = IncremntSeed;
+            Dc.Caption = Caption;
+            Dc.ReadOnly = ReadOnly;
+            Dc.Unique = Unique;
+            Table.Columns.Add(Dc);
+        
+        }
         static void Main(string[] args)
         {
 
             DataTable Employees = new DataTable();
+            CreatCoulmn(Employees, typeof(int), "ID", true, "Employee ID", false, true);
+            CreatCoulmn(Employees, typeof(string), "FirstName", false, "Employee FirstName", false, false);
+            CreatCoulmn(Employees, typeof(string), "LastName", false,  "Employee LastName", false, false);
+            CreatCoulmn(Employees, typeof(char), "Gender", false, "Gender", false, false);
+            CreatCoulmn(Employees, typeof(float), "Salary", false, "Salary Amount", false, false);
+            CreatCoulmn(Employees, typeof(DateTime), "BirthDate", false, "Employee Birth Date", false, false);
 
-            //creating the columns: 
-            Employees.Columns.Add("ID", typeof(int));
-            Employees.Columns.Add("FirstName", typeof(string));
-            Employees.Columns.Add("LastName", typeof(string));
-            Employees.Columns.Add("Gender", typeof(Char));
-            Employees.Columns.Add("Salary", typeof(float));
-            Employees.Columns.Add("DateOfBirth", typeof(DateTime));
-
-            DataColumn[]  PrimaryKey = new DataColumn[1];
+            DataColumn[] PrimaryKey = new DataColumn[1];
             PrimaryKey[0] = Employees.Columns["ID"];
             Employees.PrimaryKey = PrimaryKey;
 
-            // filling in the date 
-            Employees.Rows.Add(1, "Carlos", "Costa", 'M', 5000, new DateTime(1998, 7, 8));
-            Employees.Rows.Add(1, "Alhoa", "Costa", 'F', 500, new DateTime(2022, 5, 8));
-            Employees.Rows.Add(3, "Ricardo", "Costa", 'M', 8000, new DateTime(1989, 7, 25));
-            Employees.Rows.Add(4, "Koda", "Costa", 'M', 200, new DateTime(2025, 1, 1));
+            Employees.Rows.Add(null, "Carlos", "Costa", 'M', 5000, new DateTime(1998, 7, 9));
+            Employees.Rows.Add(null, "Alhoa", "Costa", 'F', 500, new DateTime(2022, 5, 2));
+            Employees.Rows.Add(null, "Ricardo", "Costa", 'M', 8000, new DateTime(1989, 7,23));
+            Employees.Rows.Add(null, "Koda", "Costa", 'M', 200, new DateTime(2025, 1, 20));
+
+            //creating the columns: 
+            /* Employees.Columns.Add("ID", typeof(int));
+             Employees.Columns.Add("FirstName", typeof(string));
+             Employees.Columns.Add("LastName", typeof(string));
+             Employees.Columns.Add("Gender", typeof(Char));
+             Employees.Columns.Add("Salary", typeof(float));
+             Employees.Columns.Add("DateOfBirth", typeof(DateTime));
+            */
+            /*
+             DataColumn[]  PrimaryKey = new DataColumn[1];
+             PrimaryKey[0] = Employees.Columns["ID"];
+             Employees.PrimaryKey = PrimaryKey;
+            */
+            /*
+             // filling in the date 
+             Employees.Rows.Add(1, "Carlos", "Costa", 'M', 5000, new DateTime(1998, 7, 8));
+             Employees.Rows.Add(1, "Alhoa", "Costa", 'F', 500, new DateTime(2022, 5, 8));
+             Employees.Rows.Add(3, "Ricardo", "Costa", 'M', 8000, new DateTime(1989, 7, 25));
+             Employees.Rows.Add(4, "Koda", "Costa", 'M', 200, new DateTime(2025, 1, 1));
+            */
             //filtering date
 
             //PrintList(Employees);
